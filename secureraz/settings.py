2f773862b6720654750a74dc91ef1eff4fa28db0
Amir Savand <amir@savandbros.com>
2021-04-04 11:19:49 +0300

    Fix TEMPLATES.DIRS path

--- secureraz/settings.py
+++ secureraz/settings.py
@@ -86,7 +86,7 @@ ROOT_URLCONF = 'secureraz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [str(os.path.join(BASE_DIR, "templates"))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
